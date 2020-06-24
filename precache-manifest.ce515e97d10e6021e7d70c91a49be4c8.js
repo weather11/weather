4504e54b546d8d3bcd94579d663c115dddcf2a16
weather11 <streloc84@mail.ru>
2020-06-24 23:31:30 +0400

    Updates

---
+++ precache-manifest.ce515e97d10e6021e7d70c91a49be4c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e43914cb4c58d7b559f4341e85e04f04",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "6c2d7dbe8e09c0c11753",
+    "url": "/weather/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ad43bcd42561b6d68fbc",
+    "url": "/weather/static/js/2.14fb806a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.14fb806a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c2d7dbe8e09c0c11753",
+    "url": "/weather/static/js/main.7150b612.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  }
+]);
